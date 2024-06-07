858f8c7af88673de7f1eb269c4fd5a3a9c32de85
William Luke <williamluke4@gmail.com>
2024-06-08 01:07:34 +0300

    refactor: Update frontend.js to improve chat button and dialog

--- erpnext_chatgpt/hooks.py
+++ erpnext_chatgpt/hooks.py
@@ -9,16 +9,11 @@ app_license = "MIT"
 required_apps = ["frappe/erpnext"]
 
 # Include JS and CSS files in header of desk.html
-app_include_js = "/assets/erpnext_chatgpt/js/frontend.js?v=3"
+app_include_js = "/assets/erpnext_chatgpt/js/frontend.js"
 
 # Doctype JavaScript
-doctype_js = {"OpenAI Settings": "erpnext_chatgpt/doctype/openai_settings/openai_settings.js"}
+doctype_js = {
+    "OpenAI Settings": "erpnext_chatgpt/doctype/openai_settings/openai_settings.js"
+}
 
-fixtures = [
-    {
-        "dt": "DocType",
-        "filters": [
-            ["name", "in", ["OpenAI Settings"]]
-        ]
-    }
-]
+fixtures = [{"dt": "DocType", "filters": [["name", "in", ["OpenAI Settings"]]]}]
